aabd2bfa35778a6c435f48cfdc6488f8dd106595
Amit <128979330+Welcometocodeamit@users.noreply.github.com>
2023-06-05 16:08:06 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+function scrollWindowhome(){
+    window.scrollTo(0, 0)
+}
+
+function scrollWindowaboutme(){
+    window.scrollTo(0, 661.41732283)
+}
+
+function scrollWindowskills(){
+    window.scrollTo(0, 1322.8346)
+}
+
+function scrollWindowsproject(){
+   window.scrollTo(0, 1984.2519)
+}
+
+function scrollWindowcontact(){
+    window.scrollTo(0, 2750)
+}
